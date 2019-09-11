88bb0b4c2f2e5e2039abc23e6fa22d8dfe37ced5
Miranda Hawkes <17408278+miranda-hawkes@users.noreply.github.com>
2019-09-11 18:02:34 +0100

    Colouring cards and spacing

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import javax.inject._
+import models._
 import play.api.i18n.I18nSupport
 import play.api.mvc._
 
@@ -10,6 +11,12 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents)
   with I18nSupport {
 
   val index: Action[AnyContent] = Action { implicit request =>
-    Ok(views.html.index("Your new application is ready."))
+    val cardRow = (AppInfoCard("name", "description", "author"), Seq(
+      AppVersionCard("Manage VAT Subscription Frontend", "0.1.0", "11th September 2019", Red),
+      AppVersionCard("Deregister VAT Frontend", "0.5.0", "10th September 2019", Amber),
+      AppVersionCard("VAT Summary Frontend", "0.10.0", "9th September 2019", Green)
+    ))
+
+    Ok(views.html.dashboard(Seq(cardRow, cardRow, cardRow)))
   }
 }
